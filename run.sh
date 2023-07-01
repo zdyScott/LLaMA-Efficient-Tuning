fd09ebee64041059419144e721c2f14b749d8266
Zhong Dongyu <zhong.dongyu@gloritytech.com>
2023-07-01 16:48:01 +0800

    zdy commit

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 
-cp -rf ./data/dataset_info.json /root/workspace_law/data_baichuan
-
 export CUDA_VISIBLE_DEVICES=0
+
+
+# cp -rf ./data/dataset_info.json /root/workspace_law/data_baichuan
 # python3 src/train_sft.py \
 #     --model_name_or_path /root/workspace_law/baichuan-7B \
 #     --do_train \
@@ -22,8 +23,12 @@ export CUDA_VISIBLE_DEVICES=0
 #     --lora_target W_pack \
 #     --lora_rank 32 \
 
-python3 src/cli_demo.py \
-    --model_name_or_path /root/workspace_law/baichuan-7B \
-    --checkpoint_dir /root/workspace_law/data_baichuan/output \
+# python3 src/cli_demo.py \
+#     --model_name_or_path /root/workspace_law/baichuan-7B \
+#     --checkpoint_dir /root/workspace_law/data_baichuan/output \
+#     --lora_target W_pack \
+
+python3 src/web_demo.py \
+    --model_name_or_path /root/data_baichuan/baichuan-7B \
+    --checkpoint_dir /root/zdy/LLaMA-Efficient-Tuning/output/baichuan_lora \
     --lora_target W_pack \
-    
--- src/web_demo.py
+++ src/web_demo.py
@@ -116,14 +116,6 @@ def reset_state():
 
 with gr.Blocks() as demo:
 
-    gr.HTML("""
-    <h1 align="center">
-        <a href="https://github.com/hiyouga/LLaMA-Efficient-Tuning" target="_blank">
-            LLaMA Efficient Tuning
-        </a>
-    </h1>
-    """)
-
     chatbot = gr.Chatbot()
 
     with gr.Row():
@@ -149,4 +141,6 @@ with gr.Blocks() as demo:
 
     emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
 
-demo.queue().launch(server_name="0.0.0.0", share=True, inbrowser=True)
+demo.queue().launch(server_name="0.0.0.0", server_port=6666, show_api=False, share=True, inbrowser=True)
+
+
